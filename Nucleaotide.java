b2d37350530db824a5dadc5d14a8c91ac7a67c1d
blasta <mozart.cadet>
2017-09-29 12:19:17 -0400

    Nucléo Class - get() - set()

--- Nucleaotide.java
+++ Nucleaotide.java
@@ -1,20 +1,63 @@
 package TP1PRO2;
 
+import java.util.ArrayList;
+
 public class Nucleaotide {
 
 	
-	int nbrCarbonne = 0 ; 
-	int nbrHydrogène = 0 ; 
-	int nbrAzote = 0 ; 
-	int nbrOxygène = 0 ; 
-	double masseMolaire = 0 ; 
+	private int nbrCarbonne = 0 ;
+	private int nbrHydrogene = 0 ;
+	private int nbrAzote = 0 ;
+	private int nbrOxygene = 0 ;
+	private double masseMolaire = 0 ;
 	
 	
-  public Nucleaotide(){
-		
-		
-		
+    public Nucleaotide(int nbrCarbonne , int nbrHydrogene, int nbrAzote, int nbrOxygene,double masseMolaire){
+		this.nbrCarbonne = nbrCarbonne ;
+    	this.nbrHydrogene = nbrHydrogene ;
+    	this.nbrAzote = nbrAzote ;
+    	this.nbrOxygene = nbrOxygene ;
+    	this.masseMolaire = masseMolaire ;
 	}
 
-	
+
+	public int getNbrCarbonne() {
+		return nbrCarbonne;
+	}
+
+	public void setNbrCarbonne(int nbrCarbonne) {
+		this.nbrCarbonne = nbrCarbonne;
+	}
+
+	public int getNbrHydrogene() {
+		return nbrHydrogene;
+	}
+
+	public void setNbrHydrogene(int nbrHydrogene) {
+		this.nbrHydrogene = nbrHydrogene;
+	}
+
+	public int getNbrAzote() {
+		return nbrAzote;
+	}
+
+	public void setNbrAzote(int nbrAzote) {
+		this.nbrAzote = nbrAzote;
+	}
+
+	public int getNbrOxygene() {
+		return nbrOxygene;
+	}
+
+	public void setNbrOxygene(int nbrOxygene) {
+		this.nbrOxygene = nbrOxygene;
+	}
+
+	public double getMasseMolaire() {
+		return masseMolaire;
+	}
+
+	public void setMasseMolaire(double masseMolaire) {
+		this.masseMolaire = masseMolaire;
+	}
 }
